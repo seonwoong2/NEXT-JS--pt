07e7c271d2af8d13bdcc93f33e9947ab752d5903
seonwoong <lso5936@gmail.com>
2022-01-29 17:53:45 +0900

    ErrPage fix

--- nextjs-practice/pages/404.js
+++ nextjs-practice/pages/404.js
@@ -1,7 +1,14 @@
 export default function NotFound() {
   return (
     <div>
-      <h1>{`What are you doing here?`}</h1>
+      <h1>{`404 Err`}</h1>
+      <div className='err_message'>{`존재하지 않는 URL입니다 다시 확인해 주세요`}</div>
+      <style jsx>{`
+        .err_message {
+          font-size: 20px;
+          font-weight: bold;
+        }
+      `}</style>
     </div>
   );
 }
